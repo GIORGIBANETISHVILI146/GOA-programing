2f8c383192b43420f63472dafdb8f3c37949ceaa
GIORGIBANETISHVILI146 <giorgibanetishvili59@gmail.com>
2025-10-24 21:47:12 +0400

    second commit

---
+++ day 03/hw05.py
@@ -0,0 +1,15 @@
+#შექმენით შემდეგი ცვლადები snake_case გამოყენებით:
+#1. my name
+#2. place of birth
+#3. eye color
+#ცვლადებში შესაბამისი მნიშვნელობები შეინახეთ.
+
+
+my_name = "giorgi_banetishvili"
+print(my_name)
+
+place_of_birth = "village_Martkophi"
+print(place_of_birth)
+
+eye_color = "brown"
+print(eye_color)
