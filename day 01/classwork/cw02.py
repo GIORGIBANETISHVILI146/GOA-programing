da8fb74aee110f52892d86396b2e574615a48cb1
GIORGIBANETISHVILI146 <giorgibanetishvili59@gmail.com>
2025-10-24 21:56:00 +0400

    second commit

--- day 01/classwork/cw02.py
+++ day 01/classwork/cw02.py
@@ -1,26 +1,15 @@
 #ცვლადები
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a7c27fcf5c7dfeb827beab085cfee7cb1e471cec
+
 #string
 messi = ("world cup")
 print(messi)
 
 #integer
-<<<<<<< HEAD
 year = (11)
 print(year)
 
 #float
 float = (2.3)
 print(float)
-=======
-year=(11)
-print(year)
-
-#float
-float=(2.3)
-print(float)
->>>>>>> a7c27fcf5c7dfeb827beab085cfee7cb1e471cec
