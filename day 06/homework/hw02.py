73c909500469b0d94a263c2dfbabc9b1de048af4
GIORGIBANETISHVILI146 <giorgibanetishvili59@gmail.com>
2025-11-09 21:45:50 +0400

    second commit

---
+++ day 06/homework/hw02.py
@@ -0,0 +1,9 @@
+#შექმენით 2 ცვლადი და შემდეგ შეადრეთ ერთმანეთზე
+num1 = 56
+num2 = 86
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+print(num1 <= num2)
+print(num1 >= num2)
+print(num1 != num2)
---
+++ day 06/homework/hw04.py
@@ -0,0 +1,9 @@
+#შექმენით 2 ცვლადი სადაც შეეკითხებოდით რენდომ რიცხვებს,შემდეგ კი შეადარებთ ერთმანეთს
+user_num1 = int(input("enter your number:"))
+user_num2 = int(input("enter your number:"))
+print(user_num1>user_num2)
+print(user_num1<user_num2)
+print(user_num1>=user_num2)
+print(user_num1<=user_num2)
+print(user_num1==user_num2)
+print(user_num1!=user_num2)
