c61ff1d66f97bfb10c66b4905e8b3ad8e6ddba0d
GIORGIBANETISHVILI146 <giorgibanetishvili59@gmail.com>
2026-03-10 19:11:12 +0400

    ooh

---
+++ main.py/day 21/homework/hw04.py
@@ -0,0 +1,4 @@
+# შექმენით ფუნქცია რომელსაც ერქმევა თქვენი საყვარელი მანაქნის სახელი,ფუნქციას უნდა გადაცეთ ბრენდი,ცხენის ძალა , sport car. ამის შემდეგ ეს ინფორმაციები გამოიტანეთ ეკრანზე
+def favorite_mustang(brand, horsepower, sport_car):
+    print("My favorite is the" + brand + "with " + str(horsepower) + " horsepower, and it is a" + sport_car +".")
+favorite_mustang("Mustang", 450, "sport car")
---
+++ main.py/day 21/homework/hw07.py
@@ -0,0 +1,5 @@
+# შექმენი ფუნქცია Find_letter,სადაც გადაცემ სტრინგს.შენი დავალება არის რომ ამ სტრინგიდან დაითვალო რამდენი a ურევია მასში და გამოიტანე ეკრანზე
+def find_letter(string):
+    count = string.count("a")
+    print(count)
+find_letter("aassdddafF")
